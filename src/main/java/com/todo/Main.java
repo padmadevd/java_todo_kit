a0a048e5459bc949b9b8a34c2a368ba3b61a2fa6
padmadevd <padmadev25@gmail.com>
2025-09-16 13:43:13 +0530

    commit

--- src/main/java/com/todo/Main.java
+++ src/main/java/com/todo/Main.java
@@ -1,9 +1,22 @@
-package main.java.com.todo;
+package com.todo;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.todo.util.DatabaseConnection;
 
 public class Main
 {
     public static void main(String args[])
     {
+        DatabaseConnection db_Connection = new DatabaseConnection();
 
+        try{
+            Connection cn = db_Connection.getDBConnection();
+        }
+        catch(SQLException e){
+            System.out.println("Connection Failed");
+        }
+        System.out.println("Success\n");
     }
 }
