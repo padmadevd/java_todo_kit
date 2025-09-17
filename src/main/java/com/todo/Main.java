a7e04dcce88d50f8f23a70a3370c6604781a4f69
padmadevd <padmadev25@gmail.com>
2025-09-17 14:45:03 +0530

    commit

--- src/main/java/com/todo/Main.java
+++ src/main/java/com/todo/Main.java
@@ -3,6 +3,11 @@ package com.todo;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import com.todo.gui.TodoAppGUI;
 import com.todo.util.DatabaseConnection;
 
 public class Main
@@ -16,7 +21,29 @@ public class Main
         }
         catch(SQLException e){
             System.out.println("Connection Failed");
+            System.exit(1);
         }
         System.out.println("Success\n");
+
+        try{
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }
+        catch(ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e)
+        {
+            System.err.println("Could not set look and feel "+e.getMessage());
+        }
+
+        SwingUtilities.invokeLater(
+            () -> {
+                try
+                {
+                    new TodoAppGUI().setVisible(true);
+                }
+                catch(Exception e)
+                {
+                    System.err.println("Error starting the application" + e.getLocalizedMessage());
+                }
+            }
+        );
     }
 }
